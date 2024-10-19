5bae71cd71e687ae9f4fd38f4aad9469c4dd54c2
Sasaank Janapati <sasaankwork@gmail.com>
2024-10-19 12:57:25 +0530

    changed server url

--- src/app/api/apiSlice.js
+++ src/app/api/apiSlice.js
@@ -2,7 +2,7 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 import { setCredentials } from "../../features/auth/authSlice";
 
 const baseQuery = fetchBaseQuery({
-  baseUrl: "https://clubscribes-api.onrender.com",
+  baseUrl: "https://clubscribes-backend.onrender.com",
   credentials: "include",
   prepareHeaders: (headers, { getState }) => {
     const token = getState().auth.token;
